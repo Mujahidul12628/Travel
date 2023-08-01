f8843caa3b828580efa60821d1bcfee52091c00a
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-08-01 23:40:20 +0600

    Discount deal

--- src/assets/Component/HotDeal/HotDeal.jsx
+++ src/assets/Component/HotDeal/HotDeal.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 const HotDeal = () => {
     return (
         <div>
+            <h1>Discount List</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea, laboriosam. Molestiae iure ad doloremque aspernatur, nam perferendis obcaecati eum mollitia?</p>
 
         </div>
     );
