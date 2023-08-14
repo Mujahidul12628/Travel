f3634fc568cfd41b7e97a8a805a298c7d2a0e36d
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-08-14 23:53:57 +0600

    Route setup

---
+++ src/assets/routes/Route.jsx
@@ -0,0 +1,68 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+// import RecipeDetails from "../pages/recipeDetails/RecipeDetails";
+// import ChefRecipes from "../pages/chefRecipes/ChefRecipes";
+
+// import PrivateRoute from "./PrivateRoute";
+// import Blog from "../pages/Blog/Blog";
+
+import Home from "../Component/Home/Home";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
+import MainLayout from "../Common/MainLayout/MainLayout";
+import ErrorPage from "../Common/ErrorPage/ErrorPage";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout></MainLayout>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            },
+            // {
+            //     path: "/chef/:id",
+            //     element: (
+            //         <PrivateRoute>
+            //             <ChefRecipes></ChefRecipes>
+            //         </PrivateRoute>
+            //     ),
+            //     loader: ({ params }) =>
+            //         fetch(
+            //             `http://localhost:5555/chef/${params.id}`
+            //         ),
+            // },
+            // {
+            //     path: "/recipe/:id",
+            //     element: (
+            //         <PrivateRoute>
+            //             <RecipeDetails></RecipeDetails>
+            //         </PrivateRoute>
+            //     ),
+            //     loader: ({ params }) =>
+            //         fetch(
+            //             `http://localhost:5555/recipe/${params.id}`
+            //         ),
+            // },
+            // {
+            //     path: "/blog",
+            //     element: <Blog></Blog>,
+            //     loader: () =>
+            //         fetch("http://localhost:5555/questions"),
+            // },
+            {
+                path: "/login",
+                element: <Login></Login>,
+            },
+            {
+                path: "/register",
+                element: <Register></Register>,
+            },
+        ],
+    },
+]);
+
+export default router;
