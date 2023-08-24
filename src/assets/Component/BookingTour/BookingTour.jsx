06f59912c7817f11242b7181f4ef359edab7187a
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-08-24 22:50:37 +0600

    Form data

--- src/assets/Component/BookingTour/BookingTour.jsx
+++ src/assets/Component/BookingTour/BookingTour.jsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const BookingTour = () => {
+function BookingTour() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // You can perform form submission logic here
+        console.log('Form submitted:', name, email, message);
+    };
+
     return (
         <div>
-            <h1>Travel Booking Site</h1>
+            <h1>Contact Us</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Name:</label>
+                <input
+                    type="text"
+                    id="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                />
+                <br />
+
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+                <br />
+
+                <label htmlFor="message">Message:</label>
+                <textarea
+                    id="message"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    rows="4"
+                    cols="50"
+                    required
+                />
+                <br />
+
+                <button type="submit">Submit</button>
+            </form>
         </div>
     );
-};
+}
 
 export default BookingTour;
