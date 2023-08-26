fecb5d646e810aed6411d22d3a7c80b213774889
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-08-26 22:48:28 +0600

    Blog connected with API

--- src/assets/Component/Blog/Blog.jsx
+++ src/assets/Component/Blog/Blog.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Blog = () => {
+function Blog() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = () => {
+        fetch('http://localhost:5012/reviews')
+            .then(response => response.json())
+            .then(jsonData => setData(jsonData))
+            .catch(error => console.error('Error fetching data:', error));
+    };
+
     return (
-        <div className='mx-auto max-w-7xl'>
-            <h1>Blog</h1>
+        <div>
+            <h1>Data from Backend</h1>
+            <ul>
+                {data.map(item => (
+                    <li key={item.id}>{item.userName}</li>
+                ))}
+            </ul>
         </div>
     );
-};
+}
 
 export default Blog;
