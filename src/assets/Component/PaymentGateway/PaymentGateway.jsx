2ecf15c0447a0950fa86aa94978634281442b9a6
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-09-02 23:03:20 +0600

    Payment method

---
+++ src/assets/Component/PaymentGateway/PaymentGateway.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PaymentGateway = () => {
+    return (
+        <div>
+            <h1>PAyment Method</h1>
+        </div>
+    );
+};
+
+export default PaymentGateway;
